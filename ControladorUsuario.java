72977b6189a050b8725758699a9577c25689a0eb
Rodrigo Ramirez <131787074+Guallitoprogra@users.noreply.github.com>
2023-10-06 18:46:07 -0600

    Add files via upload

---
+++ ControladorUsuario.java
@@ -0,0 +1,15 @@
+public class ControladorUsuario {
+    // Lógica relacionada con la gestión de usuarios, como registro y autenticación
+
+    public boolean registrarUsuario(String nombre, int edad) {
+        // Implementar la lógica de registro en la base de datos
+        // Retornar true si el registro es exitoso, false si falla
+        return true;
+    }
+
+    public boolean autenticarUsuario(String nombre, String contrasena) {
+        // Implementar la lógica de autenticación
+        // Retornar true si la autenticación es exitosa, false si falla
+        return true;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Crear una instancia de la ventana principal y hacerla visible
+        VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
+        ventanaPrincipal.setVisible(true);
+    }
+}
